329f1bc0f1838d15b934385da9fa00e84c0780f8
Bhavy gupta <bhavygupta2006ssilver@gmail.com>
2025-08-06 14:29:29 +0530

    arrays and statuses

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -3,11 +3,11 @@ import {Task} from '../utils/data-tasks'
 const Taskcard = ({task}: { 
     task: Task
 }) => {
-      return <div className=" border rounded-lg px-2 m-2 bg-gray-50">
-             <div className="text-base font-semibold py-2">
+      return <div className=" border rounded-lg px-2 m-2 bg-gray-50 w-60">
+             <div className="text-base font-base py-2">
                 {task.title}!!!
             </div>
-            <div className="flex gap-2 justify-between py-2 text-gray-700">
+            <div className="flex gap-2 justify-between py-2 text-gray-500 text-sm">
             <div>{task.id}</div>
             <div>{task.points}</div>
             </div>
--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -1,12 +1,20 @@
+export type Status = 'todo' | 'in-progress' | 'done'
 export type Task = {
   title: string,
+  status: Status,
   id: string,
   points?: number 
 }
 
+export const statuses: Status[] = [ 'todo', 'in-progress', 'done']
+
 export const tasks: Array<Task> = [
-    {title:"Do Market Research", id: "BUS-1", points: 5}, 
-    {title:"Competitor Analysis", id: "BUS-2"}, 
-    {title:"Develop Business Strategy", id: "BUS-3", points: 8}, 
-    {title:"Develop Marketing Strategy", id: "BUS-4", points: 5}
+    {title:"Do Market Research",status: 'todo', id: "BUS-1", points: 5}, 
+    {title:"Competitor Analysis",status: 'todo', id: "BUS-2"}, 
+    {title:"Develop Business Strategy",status: 'todo', id: "BUS-3", points: 8}, 
+    {title:"Develop Marketing Strategy",status: 'todo', id: "BUS-4", points: 5},
+    {title:"Develop Marketing Strategy",status: 'todo', id: "BUS-5", points: 5},
+    {title:"Develop Marketing Strategy",status: 'done', id: "BUS-6", points: 5},
+    {title:"Develop Marketing Strategy",status: 'in-progress', id: "BUS-7", points: 5},
+    {title:"Develop Marketing Strategy",status: 'in-progress', id: "BUS-8", points: 5}
   ] 
