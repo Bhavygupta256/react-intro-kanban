b294522bd3a1e4082e00fce43e3fc87af6b85acc
Bhavy gupta <bhavygupta2006ssilver@gmail.com>
2025-08-06 15:06:42 +0530

    priority icons

--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -1,20 +1,23 @@
 export type Status = 'todo' | 'in-progress' | 'done'
+export type Priority = 'low' | 'medium' | 'high'
 export type Task = {
   title: string,
   status: Status,
+  priority: Priority,
   id: string,
   points?: number 
 }
 
 export const statuses: Status[] = [ 'todo', 'in-progress', 'done']
+export const priorities: Priority[] = [ 'low', 'medium', 'high' ]
 
 export const tasks: Array<Task> = [
-    {title:"Do Market Research",status: 'todo', id: "BUS-1", points: 5}, 
-    {title:"Competitor Analysis",status: 'todo', id: "BUS-2"}, 
-    {title:"Develop Business Strategy",status: 'todo', id: "BUS-3", points: 8}, 
-    {title:"Develop Marketing Strategy",status: 'todo', id: "BUS-4", points: 5},
-    {title:"Develop Marketing Strategy",status: 'todo', id: "BUS-5", points: 5},
-    {title:"Develop Marketing Strategy",status: 'done', id: "BUS-6", points: 5},
-    {title:"Develop Marketing Strategy",status: 'in-progress', id: "BUS-7", points: 5},
-    {title:"Develop Marketing Strategy",status: 'in-progress', id: "BUS-8", points: 5}
+    {title:"Do Market Research",status: 'todo', priority: 'high' , id: "BUS-1", points: 5}, 
+    {title:"Competitor Analysis",status: 'todo', priority: 'high'  ,id: "BUS-2"}, 
+    {title:"Develop Business Strategy",status: 'todo', priority: 'high'  ,id: "BUS-3", points: 8}, 
+    {title:"Develop Marketing Strategy",status: 'todo', priority: 'high'  ,id: "BUS-4", points: 5},
+    {title:"Develop Marketing Strategy",status: 'todo', priority: 'high'  ,id: "BUS-5", points: 5},
+    {title:"Develop Marketing Strategy",status: 'done', priority: 'low'  ,id: "BUS-6", points: 5},
+    {title:"Develop Marketing Strategy",status: 'in-progress', priority: 'low'  ,id: "BUS-7", points: 5},
+    {title:"Develop Marketing Strategy",status: 'in-progress', priority: 'medium' , id: "BUS-8", points: 5}
   ] 
