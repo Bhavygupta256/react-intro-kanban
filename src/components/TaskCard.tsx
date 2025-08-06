46f796ef11696d742dbf52cc2436190e9eedfdb1
Bhavy gupta <bhavygupta2006ssilver@gmail.com>
2025-08-06 20:03:48 +0530

    update task points

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,4 @@
+
 import {Task} from '../utils/data-tasks'
 
 const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -13,9 +14,11 @@ const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
 </svg>
 
-const Taskcard = ({task}: { 
+const Taskcard = ({task, updateTaskPoints }: { 
     task: Task
+    updateTaskPoints: (task: Task, points: number) => void
 }) => {
+      const points = task.points || 0
       return <div className=" border rounded-lg px-2 m-2 bg-gray-50 w-60">
              <div className="text-base font-base py-2">
                 {task.title}!!!
@@ -23,12 +26,15 @@ const Taskcard = ({task}: {
             <div className="flex gap-2 justify-between py-2 text-gray-500 text-sm">
             <div className='flex gap-2'>
                 <div>{task.id}</div>
-                <div>{task.priority}</div>
                 {task.priority === 'high' && highPriorityIcon}
                 {task.priority === 'medium' && mediumPriorityIcon}
                 {task.priority === 'low' && lowPriorityIcon}
             </div>
-            <div>{task.points}</div>
+            <div className='flex gap-2 items-center'>
+                <button onClick={() => updateTaskPoints(task, points -1) } >-</button>
+                <div className='font-bold'>{points}</div>
+                <button onClick={() => updateTaskPoints(task, points + 1) } >+</button>
+            </div>
             </div>
             </div>
   }
