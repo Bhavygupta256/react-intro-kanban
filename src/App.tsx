f7cb96f0448d0ba03128bb3a72373f108687b42f
Bhavy gupta <bhavygupta2006ssilver@gmail.com>
2025-08-09 17:33:38 +0530

    connect to json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Taskcard from './components/TaskCard'
-import { tasks as initialTasks, Status, statuses, Task } from './utils/data-tasks'
+import { Status, statuses, Task } from './utils/data-tasks'
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>(initialTasks)
+  const [tasks, setTasks] = useState<Task[]>([])
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status)
     return {
-      title: status,
+      status,
       tasks: tasksInColumn
     }
 
   })
 
+  useEffect(() => {
+    console.log("setting effects")
+    fetch('http://localhost:3000/tasks')
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data)
+      })
+  }, [])
+
   const updateTask = (task: Task) => {
+    fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
     const updatedTasks = tasks.map((t) => {
       return t.id === task.id ? task : t
     })
@@ -23,27 +39,39 @@ function App() {
  
   const handleDrop = (e: React.DragEvent<HTMLDivElement> , status: Status) => {
     e.preventDefault()
+    setCurrentlyHoveringOver(null)
     const id = e.dataTransfer.getData("id")
     const task = tasks.find((task) => task.id === id)
     if(task) {
       updateTask({...task, status})
     }
   }
+  const [currentlyHoveringOver, setCurrentlyHoveringOver] = useState<Status | null>(null)
+  const handleDragEnter = (status: Status) => {
+    setCurrentlyHoveringOver(status)
+  }
 
   return (
     <div className="flex divide-x">
       {columns.map((column) => (
-        <div onDrop={(e) => handleDrop(e, column.title)} onDragOver={(e) => e.preventDefault()}>
+        <div 
+        onDrop={(e) => handleDrop(e, column.status)} 
+        onDragOver={(e) => e.preventDefault()}
+        onDragEnter={() => handleDragEnter(column.status)}
+        >
           <div className='flex justify-between text-3xl p-2 font-bold text-gray-500'>
-            <h2 className='capitalize'>{column.title}</h2>
+            <h2 className='capitalize'>{column.status}</h2>
             {column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}
           </div>
+          <div className={`h-full ${currentlyHoveringOver === column.status ? 'bg-gray-200' : ''}`}>
           {column.tasks.map((task) =>
             <
               Taskcard
               task={task}
               updateTask = {updateTask}
-            />)}
+            />
+            )}
+            </div>
         </div>
       ))}
     </div>
--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -11,13 +11,3 @@ export type Task = {
 export const statuses: Status[] = [ 'todo', 'in-progress', 'done']
 export const priorities: Priority[] = [ 'low', 'medium', 'high' ]
 
-export const tasks: Array<Task> = [
-    {title:"Do Market Research",status: 'todo', priority: 'high' , id: "BUS-1", points: 5}, 
-    {title:"Competitor Analysis",status: 'todo', priority: 'high'  ,id: "BUS-2"}, 
-    {title:"Develop Business Strategy",status: 'todo', priority: 'high'  ,id: "BUS-3", points: 8}, 
-    {title:"Develop Marketing Strategy",status: 'todo', priority: 'high'  ,id: "BUS-4", points: 5},
-    {title:"Develop Marketing Strategy",status: 'todo', priority: 'high'  ,id: "BUS-5", points: 5},
-    {title:"Develop Marketing Strategy",status: 'done', priority: 'low'  ,id: "BUS-6", points: 5},
-    {title:"Develop Marketing Strategy",status: 'in-progress', priority: 'low'  ,id: "BUS-7", points: 5},
-    {title:"Develop Marketing Strategy",status: 'in-progress', priority: 'medium' , id: "BUS-8", points: 5}
-  ] 
